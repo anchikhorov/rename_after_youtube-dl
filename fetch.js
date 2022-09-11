eafb2a7f913595800fe27f7400b71de611cf49a1
Alexander Anchikhorov <alexander.anchihorov@gmail.com>
2022-09-11 18:18:10 +0300

    startRename changed

--- fetch.js
+++ fetch.js
@@ -18,6 +18,20 @@ const de = {
   path: "/run/user/1000/gvfs/afp-volume:host=GoFlexHome.local,user=alex,volume=alex/GoFlex Home Backup/learning/Полиглот сокращённый/Deutsch"
 };
 
+const arch = {
+  playlist: "https://www.youtube.com/playlist?list=PLHhi8ymDMrQYGZLuEc92Sp0uO2fhoSslz",
+  path: "/home/alex/Videos/Software system architecture with examples in JavaScript and Node.js",
+}
+
+const patterns = {
+  playlist: "https://www.youtube.com/playlist?list=PLHhi8ymDMrQaJFrm02DoSnmvN5P-cVykm",
+  path: "/home/alex/Videos/Patterns and templates",
+}
+
+const grasp = {
+  playlist: "https://www.youtube.com/playlist?list=PLHhi8ymDMrQby8kXxsz2-J6-lsv0ilEg2",
+  path: "/home/alex/Videos/GRASP",
+}
 const fileslist = [];
 let regexp = /(.{11})(?=\.mp4$)/;
 
@@ -101,12 +115,16 @@ const startRename = (langObj) => {
     fileslist.forEach((item) => {
       const result = deepSearch(
         data,
+        //"playlistVideoRenderer",
         "videoId",
-        (k, v) => v === item.match(regexp)[0]
+        (k, v) => item.includes(v) //item.includes(v.title.runs[0].text)  //   v === item.match(regexp)[0]
+      
       );
       if (result) {
+        //console.log(result)
         fs.rename(
           `${langObj.path}/${item}`,
+          //`${langObj.path}/${result.playlistVideoRenderer.index.simpleText}_${item}`,
           `${langObj.path}/${result.index.simpleText}_${item}`,
           (err) => {
             if (err) throw err;
@@ -118,4 +136,4 @@ const startRename = (langObj) => {
   });
 };
 
-startRename(en);
+startRename(grasp);
